94a6a4c7ecb1c1d901e7fb9b88b17bb158dfd4f6
Vlado <vladmsu@ukr.net>
2015-11-11 14:33:21 +0200

    3835 -begining

---
+++ app/code/local/Tim/Tauron/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Tim
+ *
+ * @category   Tim
+ * @package    Tim_Tauron
+ * @copyright  Copyright (c) 2015 Tim (http://tim.pl)
+ * @author     Vladislav Verbitskiy <vladomsu@gmail.com>
+ */
+class Tim_Tauron_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
