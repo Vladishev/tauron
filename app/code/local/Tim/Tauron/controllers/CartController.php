53b23d9a5614fe1322181cc233eed3675b1ff7ff
Vlado <vladmsu@ukr.net>
2015-11-12 13:51:09 +0200

    3835 - CR

--- app/code/local/Tim/Tauron/controllers/CartController.php
+++ app/code/local/Tim/Tauron/controllers/CartController.php
@@ -9,29 +9,35 @@
  */
 class Tim_Tauron_CartController extends Mage_Core_Controller_Front_Action
 {
-    public function indexAction()
+    /**
+     * Uncomment and put your data for test.
+     * After this put http//:tauron.local/tim_tauron/cart/ to url field.
+     */
+    /*public function indexAction()
     {
         $firstName = 'Vasya';
         $lastName = 'Pupkin';
         $ppe = 'ppe';
         $email = 'test@gmail.com';
         $phone = '3880080800';
-        $sku = 'P2';
+        $sku = 'Bundle Prodyct';
         $nip = 'nip';
         $address = 'address';
         $customerId = 'customerId';
         $employee = 'employee';
-        $md5 = md5($firstName.$lastName.$ppe.$email.$phone.$sku.$nip.$address.$customerId.$employee);
+        $salt = Mage::helper('tim_tauron')->getSalt();
+        $md5 = md5($firstName.$lastName.$ppe.$email.$phone.$sku.$nip.$address.$customerId.$employee.$salt);
         $requestString = 'first_name='.$firstName.'&last_name='.$lastName.'&ppe='.$ppe.'&email='.$email.'&phone='.$phone.'&sku='.$sku.'&nip='.$nip.'&address='.$address.'&id_customer='.$customerId.'&employee='.$employee.'&md5='.$md5;
         $encodedString = base64_encode($requestString);
-
         $url=Mage::getUrl("tim_tauron/cart/decode", array('request'=>$encodedString));
+
         $this->_redirectUrl($url);
+    }*/
 
-//        $url = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB).'tim_tauron'.DS.'cart'.;
-        echo $firstName.$lastName.$ppe.$email.$phone.$sku.$nip.$address.$customerId.$employee;
-    }
-
+    /**
+     * Decode data from url string, check and send it to checkout/cart.
+     * Show popup window if something wrong.
+     */
     public function decodeAction()
     {
         $data = '';
@@ -45,18 +51,18 @@ class Tim_Tauron_CartController extends Mage_Core_Controller_Front_Action
             }
             $data .= $value;
         }
-        $checkMd5 = md5($data);
+        $salt = Mage::helper('tim_tauron')->getSalt();
+        $checkMd5 = md5($data.$salt);
         if ($requestData['md5'] == $checkMd5) {
-
-            $id = '2'; // Replace id with your product id
-            $qty = '1'; // Replace qty with your qty
-            $_product = Mage::getModel('catalog/product')->load($id);
+            $product_id = Mage::getModel("catalog/product")->getIdBySku($requestData['sku']);
+            $qty = '1';
+            $_product = Mage::getModel('catalog/product')->load($product_id);
 
             $cart = Mage::getModel('checkout/cart');
             $cart->init();
 
             $params = array(
-                'product'=>$id,
+                'product'=>$product_id,
                 'qty' => $qty,
             );
 
@@ -68,8 +74,7 @@ class Tim_Tauron_CartController extends Mage_Core_Controller_Front_Action
             Mage::getSingleton('checkout/session')->setCartWasUpdated(true);
             $this->_redirect('checkout/cart');
         } else {
-            echo 'ERROR!!!';
+            echo 'ERROR!!!';//here should be popup functionality.
         }
-//        print_r($data);
     }
 }
