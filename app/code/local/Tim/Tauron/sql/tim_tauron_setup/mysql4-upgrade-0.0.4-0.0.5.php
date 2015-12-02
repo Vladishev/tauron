d7c683912d3a64aaa538b48eea8a149ac522e263
Vlado <vladmsu@ukr.net>
2015-12-02 20:38:12 +0200

    3994 - for test

---
+++ app/code/local/Tim/Tauron/sql/tim_tauron_setup/mysql4-upgrade-0.0.4-0.0.5.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Tim
+ *
+ * @category   Tim
+ * @package    Tim_Tauron
+ * @copyright  Copyright (c) 2015 Tim (http://tim.pl)
+ * @author     Vladislav Verbitskiy <vladomsu@gmail.com>
+ */
+
+$installer = $this;
+
+$installer->startSetup();
+Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+$staticBlock =  array(
+    'title' => 'Popup',
+    'identifier' => 'tauron-popup',
+//        'content' => '',
+    'is_active' => 1,
+    'stores' => array(0),
+);
+
+Mage::getModel('cms/block')->setData($staticBlock)->save();
+
+$installer->endSetup();
