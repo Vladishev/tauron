3bc57165d74130301ede4a2aeead61a7347dff0a
Krzysztof Drobnik <k.drobnik@tim.pl>
2015-12-07 21:56:31 +0100

    protect before use the same URL

--- app/code/core/Mage/Customer/controllers/AccountController.php
+++ app/code/core/Mage/Customer/controllers/AccountController.php
@@ -124,15 +124,17 @@ class Mage_Customer_AccountController extends Mage_Core_Controller_Front_Action
      */
     public function loginAction()
     {
-        if ($this->_getSession()->isLoggedIn()) {
-            $this->_redirect('*/*/');
-            return;
-        }
-        $this->getResponse()->setHeader('Login-Required', 'true');
-        $this->loadLayout();
-        $this->_initLayoutMessages('customer/session');
-        $this->_initLayoutMessages('catalog/session');
-        $this->renderLayout();
+//        if ($this->_getSession()->isLoggedIn()) {
+//            $this->_redirect('*/*/');
+//            return;
+//        }
+//        $this->getResponse()->setHeader('Login-Required', 'true');
+//        $this->loadLayout();
+//        $this->_initLayoutMessages('customer/session');
+//        $this->_initLayoutMessages('catalog/session');
+//        $this->renderLayout();
+         $this->_redirect('https://www.jasnekorzysci.pl/produkty/pakiet-led.html');
+        
     }
 
     /**
