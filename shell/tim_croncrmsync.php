d1302852ce1b7f05234e25946be96914daaf3956
Vlado <vladmsu@ukr.net>
2015-12-18 15:34:15 +0200

    4107 - added tim_croncrmsync.php

---
+++ shell/tim_croncrmsync.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Tim
+ *
+ * @category   Tim
+ * @package    Tim_Tauron
+ * @copyright  Copyright (c) 2015 Tim (http://tim.pl)
+ * @author     Vladislav Verbitskiy <vladomsu@gmail.com>
+ */
+require 'app/Mage.php';
+ini_set('display_errors', 1);
+Mage::app();
+$croncrmsync = Mage::getModel('croncrmsync/croncrmsync');
+$croncrmsync->crmsynchronization();
+exit;
